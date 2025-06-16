4f9b119c3aa4550e33cbe7aa51de550d29d46223
avackaamicheal <avackaamicheal@gmail.com>
2025-06-16 13:12:57 +0100

    final touches

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from models import User, Event, Speaker, Registration
 
-users: list[User] = [{"id": 1, "name": "Alice", "email": "c@gmail.com", "is_active": True}]
+users: list[User] = []
 events: list[Event] = []
 speakers: list[Speaker] = [
     Speaker(id=1, name="Rotimi Akanni", topic="Full Backend with Python and FastApi"),
@@ -8,4 +8,4 @@ speakers: list[Speaker] = [
     Speaker(id= 2, name= "Prof. Michael Chen", topic= "Blockchain Fundamentals"),
 ]
 
-registrations:list[Registration]= [{"user_id": 1, "event_id": 1, "attended": True}]
+registrations:list[Registration]= []
